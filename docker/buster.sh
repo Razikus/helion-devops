66e86721ebfd1ff0d9cfd833bcdaf7d8981ae981
Adam Raźniewski <adam.razniewski@gmail.com>
2019-08-28 22:11:13 +0200

    Create buster.sh

---
+++ docker/buster.sh
@@ -0,0 +1,11 @@
+ apt update
+ apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+ curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+ add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+deb [arch=amd64] https://download.docker.com/linux/debian buster stable
+apt update
+apt -y install docker-ce docker-compose
+usermod -aG docker $USER
